417098b72f1c9700426d34e5cd96763961e7ee9b
uvesh patel <uveshpatelgit@gmail.com>
2024-07-25 00:13:46 +0530

    qr code view

--- derp/library/models.py
+++ derp/library/models.py
@@ -2,7 +2,6 @@ from django.db import models
 import uuid
 from io import BytesIO
 from django.core.files import File
-import qrcode
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
@@ -34,27 +33,12 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
     isbn =models.CharField('ISBN',max_length=13,unique=True)
     category = models.ManyToManyField(Category, help_text="Select a genre for this book")
-    qr_code=models.ImageField(upload_to='qr_codes',blank=True,null=True)
 
 
     def __str__(self):
         return self.title
     
-    def generate_qr_code(self):
-        qr=qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
-        )
-        qr.add_data(f'http://your-library-domain.com/books/{self.isbn}')
-        qr.make(fit=True)
-
-        img = qr.make_image(fill='black', back_color='white')
-        buffer = BytesIO()
-        img.save(buffer, format="PNG")
-        file_name = f'{self.isbn}.png'
-        self.qr_code.save(file_name, File(buffer), save=False)
+ 
 
 
 class Checkout(models.Model):
--- derp/library/views.py
+++ derp/library/views.py
@@ -1,3 +1,27 @@
+import qrcode
+from io import BytesIO
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import Book 
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+class GenerateQRcode(APIView):
+      def generate_qr_code(request, isbn):
+        book = get_object_or_404(Book, isbn=isbn)
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(f'http://127.0.0.1:8000/admin/library/book/{book.isbn}')
+        qr.make(fit=True)
+
+        img = qr.make_image(fill='black', back_color='white')
+        buffer = BytesIO()
+        img.save(buffer, format="PNG")
+        buffer.seek(0)
+
+        return HttpResponse(buffer, content_type='image/png') 
